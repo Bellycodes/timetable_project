ed3f12be8331c53d0a9999082d8c1b50992a9c8f
Adeniran John <adeniranjohn2016@gmail.com>
2023-02-13 19:32:31 +0100

    corrected

--- base/views.py
+++ base/views.py
@@ -135,7 +135,7 @@ NUMB_OF_ELITE_SCHEDULES = 1
 TOURNAMENT_SELECTION_SIZE = 3
 MUTATION_RATE = 0.05
 
-class data:
+class Data:
     def __init__(self):
         self.lectureroom = Lectureroom.objects.all()
         self.time = Time.objects.all()
@@ -156,7 +156,7 @@ class data:
 
 class Schedule:
     def __init__(self):
-        self._data = data
+        self._data = Data
         self._classes = []
         self._numberOfConflicts = 0
         self._fitness = -1
@@ -284,8 +284,8 @@ class GeneticAlgorithm:
         return tournament_pop
 
 class Class:
-    def __init__(self, id, dept, Course, coursecode):
-        # self.Course.id = id
+    def __init__(self, id, dept, coursecode):
+        self.id = id # this line
         self.department = dept
         self.coursecode = coursecode
         self.courselecturer = None
@@ -293,7 +293,7 @@ class Class:
         self.lectureroom = None
         self.course = Course 
 
-    def get_id(self): return self.course.id
+    def get_id(self): return self.id
 
     def get_dept(self): return self.department
 
@@ -312,7 +312,7 @@ class Class:
     def set_lectureroom(self, lectureroom): self.lectureroom = lectureroom
 
 
-data = data()
+data = Data()
 
 
 def context_manager(schedule):
