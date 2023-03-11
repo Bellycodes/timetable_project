e6c4b330cad79bc812d5f00cb3dfc741af961db9
Adeniran John <adeniranjohn2016@gmail.com>
2023-03-11 14:54:22 +0100

    core app

--- TimeTable/settings.py
+++ TimeTable/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# settings.py
 
 # Application definition
 
@@ -39,11 +40,27 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    'rest_framework',
+    
+    "crispy_forms",
+    "crispy_bootstrap5",
+    
     "accounts",
+    'ttgen',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'backend.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+LOGIN_URL='account:login'
+LOGIN_REDIRECT_URL = 'ttgen:dashboard' 
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,8 +137,16 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Crispy settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- TimeTable/urls.py
+++ TimeTable/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from TimeTable import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path('', include('ttgen.urls', namespace="ttgen")),
+    path('account/', include("accounts.urls", namespace="account")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
